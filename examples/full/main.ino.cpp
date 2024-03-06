7d09548782db1aa0523c895a90554cd0810be7e5
faizannazir <faizannazir289@gmail.com>
2024-03-06 08:59:47 +0500

    added my esp32

---
+++ examples/full/main.ino.cpp
@@ -0,0 +1,41 @@
+# 1 "C:\\Users\\HP840G~1\\AppData\\Local\\Temp\\tmptcmk4rqz"
+#include <Arduino.h>
+# 1 "E:/downloads/Esp32 Ble Ota/ArduinoBleOTA/examples/full/main.ino"
+#include <ArduinoBleOTA.h>
+#include <BleOtaSecurityOnConnect.h>
+
+#define DEVICE_NAME "ArduinoBleOTA"
+
+#ifdef ARDUINO_ARCH_ESP32
+  #define HW_NAME "Example ESP32"
+#elif ARDUINO_ARCH_ATMELSAM
+  #define HW_NAME "Example ATMELSAM"
+#else
+  #define HW_NAME "Example HW"
+#endif
+
+#define HW_VER {1, 0, 0}
+#define SW_NAME "Example SW"
+#define SW_VER {1, 0, 0}
+
+#ifdef USE_NIM_BLE_ARDUINO_LIB
+BleOtaSecurityOnConnect security;
+#endif
+void setup();
+void loop();
+#line 22 "E:/downloads/Esp32 Ble Ota/ArduinoBleOTA/examples/full/main.ino"
+void setup() {
+  ArduinoBleOTA.begin(DEVICE_NAME, InternalStorage, HW_NAME, HW_VER, SW_NAME, SW_VER);
+
+#ifdef USE_NIM_BLE_ARDUINO_LIB
+  ArduinoBleOTA.setSecurityCallbacks(security);
+  security.begin();
+#endif
+}
+
+void loop() {
+#ifdef USE_ARDUINO_BLE_LIB
+  BLE.poll();
+#endif
+  ArduinoBleOTA.pull();
+}
