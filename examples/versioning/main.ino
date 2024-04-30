f1ed6f2c10dacb17684d70bfcb9663fc03c45543
Embeddronics <info@embeddronics.com>
2024-04-30 13:02:13 +0500

    Updated 1.4.0 with blink code

--- examples/versioning/main.ino
+++ examples/versioning/main.ino
@@ -1,23 +1,25 @@
 #include <ArduinoBleOTA.h>
 
-#define DEVICE_NAME "ArduinoBleOTA"
+#define DEVICE_NAME "Esp32 BLE OTA"
 #define HW_NAME "Esp32 HW"
 #define HW_VER {1, 0, 0}
 #define SW_NAME "Esp32 SW"
 #define SW_VER {1, 4, 0}
 #define LED_PIN 2
+
 void setup() {
   ArduinoBleOTA.begin(DEVICE_NAME, InternalStorage, HW_NAME, HW_VER, SW_NAME, SW_VER);
-  pinMode(LED_PIN,INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-#ifdef USE_ARDUINO_BLE_LIB
-  BLE.poll();
-#endif
+  digitalWrite(LED_PIN, HIGH);
+  delay(500); // Adjust delay for desired blink rate
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+
+// #ifdef USE_ARDUINO_BLE_LIB
+//   BLE.poll();
+// #endif
   ArduinoBleOTA.pull();
-digitalWrite(LED_PIN,HIGH);
-delay(1000);
-digitalWrite(LED_PIN,LOW);
-delay(1000);
 }
